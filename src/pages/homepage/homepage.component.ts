9f3bb301bf36752efe49b3e426b63a72f89021f6
MicahDitto <35197276+MicahDitto@users.noreply.github.com>
2023-07-26 21:02:20 -0400

    4 files modified, 3 added

---
+++ src/pages/homepage/homepage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pages/homepage',
+  templateUrl: './pages/homepage.component.html',
+  styleUrls: ['./pages/homepage.component.css']
+})
+export class Pages/homepageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
